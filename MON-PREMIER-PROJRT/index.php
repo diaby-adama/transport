a8d265388d164ccd701e0902684e774de5833f08
diaby-adama <diabyadma12@gmail.com>
2025-05-03 11:37:57 +0000

    Add files via upload

---
+++ MON-PREMIER-PROJRT/index.php
@@ -0,0 +1,18 @@
+<?php 
+
+$feu = "orange";
+if($feu=="rouge"){
+    echo "les pieton passe";
+}
+elseif($feu=="vert"){
+    echo "les vehicule passe";
+}
+elseif($feu=="orange"){
+    echo "les vehicule ralentises";
+}
+else{
+    echo "le feu est en parne";
+}
+
+?>
+
